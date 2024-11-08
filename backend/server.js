3f0f09516c2833004d2aa4e752b66e9cacafa4d2
webofayush <webofayush7@gmail.com>
2024-11-08 20:50:02 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,24 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import friendRequestRoutes from "./routes/friendRequest.routes.js";
-import messageRoutes from "./routes/message.routes.js"
+import messageRoutes from "./routes/message.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import cors from "cors";
+import { app, server } from "./socket/index.js";
 import path from "path";
-import {app, server} from "./socket/index.js"
+
+const __dirname = path.resolve()
+
+
 dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 const PORT = process.env.PORT;
 const corsOptions = {
@@ -32,7 +35,7 @@ app.use(cors(corsOptions));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/friend-request", friendRequestRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/client/dist")));
